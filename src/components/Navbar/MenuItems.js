b29a5197c1cfd5ee32c7aaec6b11e30e5f19761a
Rick Barros <rickbarros@icloud.com>
2020-12-14 21:22:32 -0500

    Added files

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,29 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: 'home',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Login',
+        url: 'login',
+        cName: 'nav-links'
+    },
+    
+    {
+        title: 'About',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Register',
+        url: 'register',
+        cName: 'nav-links'
+    },
+    {
+        title: 'ContactUs',
+        url: '#',
+        cName: 'nav-links'
+    },
+   
+]
